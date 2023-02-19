4207ed6633e7d7c236d5340ce1aaf68a07ea8465
Codanaut <cody@codanaut.com>
2023-02-18 22:46:34 -0700

    update weather, removed old stuff

--- configmonitors.sh
+++ configmonitors.sh
@@ -1,3 +1,3 @@
 xrandr --output VGA-1-1 --auto --left-of HDMI-2 ;
 
-xrandr --output HDMI-1-2 --auto --right-of HDMI-2
+xrandr --output DVI-1 --auto --right-of HDMI-2
--- scripts/weather.py
+++ scripts/weather.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url = "http://api.openweathermap.org/data/2.5/weather?id=5697383&APPID=b9dbc4a81bf8f340e5a2a2dab14d6940&units=imperial"
+url = "http://api.openweathermap.org/data/2.5/weather?id=5765751&APPID=b9dbc4a81bf8f340e5a2a2dab14d6940&units=imperial"
 
 response = requests.request("GET", url)
 data = response.json()
