d747cc5d755b3db5bf2d9e115a2eceeb86d4f96b
Codanaut <cody@codanaut.com>
2023-02-20 00:41:05 -0700

    added more work spaces to switcher

--- workspaceSwitcher.sh
+++ workspaceSwitcher.sh
@@ -11,7 +11,7 @@ commandSignal="$2"
 
 if [[ "$appName" = "terminal" ]]; then
     if [[ "$commandSignal" = "1" ]]; then
-        i3-msg -q workspace "8: "
+        i3-msg -q workspace "8:"
     elif [[ "$commandSignal" = "3" ]]; then
         i3-msg exec -q konsole
     else
@@ -22,10 +22,31 @@ elif [[ "$appName" = "files" ]]; then
     if [[ "$commandSignal" = "1" ]]; then
         i3-msg -q workspace ""
     elif [[ "$commandSignal" = "3" ]]; then
-        i3-msg exec -q dolphin & disown
+        i3-msg exec -q dolphin
     else
         pass
     fi
+
+elif [[ "$appName" = "discord" ]]; then
+    if [[ "$commandSignal" = "1" ]]; then
+        i3-msg -q workspace "7:"
+    elif [[ "$commandSignal" = "3" ]]; then
+        i3-msg exec -q Discord
+    else
+        pass
+    fi
+
+elif [[ "$appName" = "browser" ]]; then
+    if [[ "$commandSignal" = "1" ]]; then
+        i3-msg -q workspace "1:"
+    elif [[ "$commandSignal" = "3" ]]; then
+        i3-msg -q workspace "4: "
+    elif [[ "$commandSignal" = "2" ]]; then
+        i3-msg exec -q firefox
+    else
+        pass
+    fi
+
 else
     pass
 fi
