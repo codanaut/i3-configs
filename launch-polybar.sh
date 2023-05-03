d4f4e3ad11e2b29ee5b64f9d9f9adb91107516a0
codanaut <cody@codanaut.com>
2023-05-03 00:12:56 -0600

    added polybar

---
+++ launch-polybar.sh
@@ -0,0 +1,13 @@
+
+# Terminate already running bar instances
+killall -q polybar
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar -r --config=~/Git/Gitlab/i3-configs/polybar.ini &
+    MONITOR=$m polybar -r --config=~/Git/Gitlab/i3-configs/polybar-bottom.ini &
+  done
+else
+  polybar -r --config=~/Git/Gitlab/i3-configs/polybar.ini &
+  MONITOR=$m polybar -r --config=~/Git/Gitlab/i3-configs/polybar-bottom.ini &
+fi
