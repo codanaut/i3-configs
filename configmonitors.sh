1ec26d32dd2a242a198f6deaa7a912773a62f6a7
codanaut <cody@codanaut.com>
2024-02-12 12:10:32 -0700

    removed old stuff

--- configmonitors.sh
+++ configmonitors.sh
@@ -1,3 +1,3 @@
-xrandr --output VGA-1-1 --auto --left-of HDMI-2 ;
+xrandr --output VGA-1-1 --auto --right-of HDMI-2 ;
 
-xrandr --output DVI-1 --auto --right-of HDMI-2
+xrandr --output DVI-0 --auto --left-of HDMI-2
